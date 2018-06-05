3decda5acddb3066bd64aa3c17a55bf712169318
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-05 23:18:52 +0200

    Matching | update http calls route further to BackEnd amendment in Routes

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-//on va créer une instance d'Axios, un nouvel objet axios , 
-//dans lequel on peut configurer des options
-
 let http = axios.create({
-	baseURL: 'http://localhost:2707',
-	// headers: {
- //    'token': sessionStorage.getItem('token')
- //  }
+	baseURL: 'http://localhost:2707/client',
 })
+
 
 export default http;
