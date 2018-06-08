fbac26b367063cffa5afa54c00a6012ab8415db8
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-08 07:54:13 +0200

    fix url

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 let http = axios.create({
 	// baseURL: 'http://localhost:2707/client',
-	baseURL: 'https://mybookingappback.herokuapp.com/client'
+	// baseURL: 'https://mybookingappback.herokuapp.com/client'
+	baseURL: 'https://mybookingappback.herokuapp.com/'
 })
 
 
