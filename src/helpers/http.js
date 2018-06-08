54b429db28654aacbbf8836ef640ec8c1bcf0f09
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-08 08:11:31 +0200

    fix url

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let http = axios.create({
 	// baseURL: 'http://localhost:2707/client',
 	// baseURL: 'https://mybookingappback.herokuapp.com/client'
-	baseURL: 'https://mybookingappback.herokuapp.com/'
+	baseURL: 'https://mybookingappback.herokuapp.com'
 })
 
 
--- src/modules/components/calendar.vue
+++ src/modules/components/calendar.vue
@@ -96,7 +96,7 @@ export default {
     },
 	},
 	created(){
-		http.get('/client')
+		http.get('client')
     .then( 
       res => {
         console.log('res from get calendar:', res);
