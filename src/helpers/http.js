020a19f107cc90f4a7ce2d85a2f7e4be601106ee
Sandrine Pradier <labelleaffranchie@gmail.com>
2018-06-09 09:42:50 +0200

    init branch dev

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 let http = axios.create({
-	// baseURL: 'http://localhost:2707/client',
-	// baseURL: 'https://mybookingappback.herokuapp.com/client'
-	baseURL: 'https://mybookingappback.herokuapp.com'
+	baseURL: 'http://localhost:2707/client',
+	// baseURL: 'https://mybookingappback.herokuapp.com'
 })
 
 http.interceptors.response.use(res => {
